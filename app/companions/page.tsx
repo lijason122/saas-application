19bea66081a4f6f924a74f241d80db72d806be2d
JASON-LI-LAPTOP\jsnli <lijason@sheridancollege.ca>
2025-09-08 17:18:39 -0400

    Add search input and filter options for filtering the companions on the companion library page

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,6 +1,31 @@
-const Companions = () => {
+import CompanionCard from "@/components/CompanionCard";
+import { getAllCompanions } from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
+import SearchInput from "./SearchInput";
+import SubjectFilter from "./SubjectFilter";
+
+const Companions = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : '';
+  const topic = filters.topic ? filters.topic : '';
+
+  const companions = await getAllCompanions({ subject, topic });
+
   return (
-    <div>Companions</div>
+    <main>
+      <section className="flex justify-between gap-4 max-sm:flex-col">
+        <h1>Companion Library</h1>
+        <div className="flex gap-4">
+          <SearchInput />
+          <SubjectFilter />
+        </div>
+      </section>
+      <section className="companion-grid">
+        {companions.map((companion) => (
+          <CompanionCard key={companion.id} color={getSubjectColor(companion.subject)} { ...companion } />
+        ))}
+      </section>
+    </main>
   )
 }
 
