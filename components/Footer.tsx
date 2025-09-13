38f7c98ff747876dc30c97211d291b611619b3cd
JASON-LI-LAPTOP\jsnli <lijason@sheridancollege.ca>
2025-09-13 16:16:40 -0400

    Add footer

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p className="footer-text">© 2025 Jason Li</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
