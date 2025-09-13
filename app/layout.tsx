53750fe546fc0ca1551d350d8d117651a6bf8ec2
JASON-LI-LAPTOP\jsnli <lijason@sheridancollege.ca>
2025-09-13 16:17:14 -0400

    Use footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Bricolage_Grotesque } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -25,6 +26,7 @@ export default function RootLayout({
         <body className={`${bricolage.variable} antialiased`}>
           <Navbar />
           {children}
+          <Footer />
         </body>
       </html>
     </ClerkProvider>
