4f3fa3ed10e420b215132e77b1aaa23f8b008845
JASON-LI-LAPTOP\jsnli <lijason@sheridancollege.ca>
2025-09-18 17:34:16 -0400

    Enable bookmark functionality on Companion Cards

--- components/CompanionCard.tsx
+++ components/CompanionCard.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { addBookmark, removeBookmark } from "@/lib/actions/companion.actions";
 
 interface CompanionCardProps {
   id: string;
@@ -8,15 +12,25 @@ interface CompanionCardProps {
   subject: string;
   duration: number;
   color: string;
+  bookmarked: boolean;
 }
 
-const CompanionCard = ({ id, name, topic, subject, duration, color }: CompanionCardProps) => {
+const CompanionCard = ({ id, name, topic, subject, duration, color, bookmarked }: CompanionCardProps) => {
+    const pathname = usePathname();
+    const handleBookmark = async () => {
+      if (bookmarked) {
+        await removeBookmark(id, pathname);
+      } else {
+        await addBookmark(id, pathname);
+      }
+    };
+
     return (
         <article className="companion-card" style={{ backgroundColor: color }}>
         <div className="flex items-center justify-between">
             <div className="subject-badge">{subject}</div>
-            <button className="companion-bookmark">
-                <Image src="/icons/bookmark.svg" alt="bookmark" width={12.5} height={15} />
+            <button className="companion-bookmark" onClick={handleBookmark}>
+                <Image src={bookmarked ? "/icons/bookmark-filled.svg" : "/icons/bookmark.svg"} alt="bookmark" width={12.5} height={15} />
             </button>
         </div>
 
